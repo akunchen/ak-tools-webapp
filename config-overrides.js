cda272b45f5a86af0d3c64282a93226591cb80a7
akun <1090093077@qq.com>
2019-10-27 23:40:36 +0800

    add less and beautify layout

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,13 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css',
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    // modifyVars: { '@primary-color': '#1DA57A' }, # 这里不注释掉，那你的无法修改主题色primary-color 这里很坑的 要注意！
   }),
 );
--- src/layouts/main/MainLayout.tsx
+++ src/layouts/main/MainLayout.tsx
@@ -1,27 +1,89 @@
 import React, { Component } from "react";
-import { Link, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import { Layout, Menu } from "antd";
+
 import HomePage from "../../pages/home/HomePage";
 import AboutPage from "../../pages/about/AboutPage";
 import MobxPage from "../../pages/mobx/MobxPage";
+import { BaseComponentProps } from "../../base/BaseComponentProps";
 
-export default class MainLayout extends Component {
+import './MainLayout.less'
+
+interface RouteItem {
+  name: string;
+  url: string;
+  component: typeof Component;
+}
+
+interface MainLayoutState {
+  routes: Array<RouteItem>;
+  clientHeight: number;
+}
+
+export default class MainLayout extends Component<BaseComponentProps, MainLayoutState> {
+
+  state = {
+    routes: [
+      { name: 'mobx', url: '/mobx', component: MobxPage },
+      { name: 'about', url: '/about', component: AboutPage },
+      { name: 'home', url: '/', component: HomePage },
+    ],
+    get clientHeight() {
+      return document.body.clientHeight;
+    }
+  };
+
+  private clickNavItem({ url }: RouteItem) {
+    this.props.history.push(url)
+  }
+
+  private renderMenuItem(r: RouteItem) {
+    return (
+      <Menu.Item key={r.name} onClick={this.clickNavItem.bind(this, r)}>
+        {r.name}
+      </Menu.Item>
+    )
+  }
+
+  private renderMenus() {
+    const url = this.props.history.location.pathname;
+    const { name } = this.state.routes.find(r => r.url === url) || { name: 'home' }
+
+    return (
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={[name]}
+        style={{ lineHeight: '64px' }}
+      >
+        {this.state.routes.map(this.renderMenuItem.bind(this))}
+      </Menu>
+    )
+  }
+
+  private renderRoutes() {
+    return (
+      <Switch >
+        {this.state.routes.map(r => <Route exact key={r.name} path={r.url} component={r.component} />)}
+      </Switch >
+    )
+  }
 
   render() {
     return (
-      <>
-        <h1>main layout</h1>
-        <ul>
-          <li><Link to="/mobx">mobx</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/">Home</Link></li>
-        </ul>
-
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/about" component={AboutPage} />
-          <Route exact path="/mobx" component={MobxPage} />
-        </Switch>
-      </>
+      <Layout className="main-layout">
+        <Layout.Header className="main-layout-header">
+          {this.renderMenus()}
+        </Layout.Header>
+        <Layout.Content className="main-layout-content" style={{ height: this.state.clientHeight }}>
+          <Layout>
+            {/* <Layout.Sider>Sider</Layout.Sider> */}
+            <Layout.Content className="">
+              {this.renderRoutes()}
+            </Layout.Content>
+          </Layout>
+        </Layout.Content>
+      </Layout>
     )
   }
 }
